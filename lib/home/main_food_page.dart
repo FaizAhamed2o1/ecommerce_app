ca4f9efee0376db2704137f03a908e9a985eaabf
FaizAhamed2o1 <faizahmed23014@gmail.com>
2023-01-08 23:55:26 +0600

    first commit

---
+++ lib/home/main_food_page.dart
@@ -0,0 +1,63 @@
+import 'package:ecommerce_app/utils/colors.dart';
+import 'package:ecommerce_app/widgets/big_text.dart';
+import 'package:ecommerce_app/widgets/small_text.dart';
+import 'package:flutter/material.dart';
+
+class MainFoodPage extends StatefulWidget {
+  const MainFoodPage({super.key});
+
+  @override
+  State<MainFoodPage> createState() => _MainFoodPageState();
+}
+
+class _MainFoodPageState extends State<MainFoodPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            child: Container(
+              margin: EdgeInsets.only(top: 30, bottom: 15),
+              padding: EdgeInsets.only(left: 15, right: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      BigText(
+                        text: 'Bangladesh',
+                        color: AppColors.mainColor,
+                      ),
+                      Row(
+                        children: [
+                          SmallText(
+                            text: 'Narsingdi',
+                            color: Colors.black54,
+                          ),
+                          Icon(Icons.arrow_drop_down_rounded),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Container(
+                    width: 45,
+                    height: 45,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: AppColors.mainColor,
+                    ),
+                    child: Icon(
+                      Icons.search_outlined,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
